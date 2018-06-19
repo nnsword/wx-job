f48da847cfbb2f5a57e3c19f71fea4c6dd4d9d93
nnsword <wuyy@xo68.cn>
2018-06-19 10:58:15 +0800

    模块信息配置

---
+++ wx-job-web/src/main/java/cn/xo68/job/web/common/QuartzContants.java
@@ -0,0 +1,12 @@
+package cn.xo68.job.web.common;
+
+/**
+ * Quartz常量
+ */
+public class QuartzContants {
+
+    /**
+     * 任务数据Key
+     */
+    public static String DATAMAP_KEY_JOB="quartzJob";
+}
